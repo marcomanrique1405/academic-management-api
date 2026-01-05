d8050e8b922588addf13de698a5f25bb2c41fc47
Marco Manrique <marcoantoniomanriquecastro@gmail.com>
2026-01-04 23:03:49 -0600

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/manrique/academic/management/AcademicManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.manrique.academic.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicManagementApiApplication.class, args);
+	}
+
+}
